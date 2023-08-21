222b582d7c7cafaca37c2291d95d5da1bc056e0c
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-08-21 11:51:08 +0200

    __functionify__, use the if-name-main idiom

--- src/maps.py
+++ src/maps.py
@@ -13,86 +13,52 @@ VERSION = '0.1-alpha'
 BWRAP = '/home/akaushik/Programs/git/Github/bubblewrap/bwrap'
 OSTREE_REPO_MODE_BARE_USER = 2
 
+
 # Define a CLI
-parser = argparse.ArgumentParser(
-    prog='maps',
-    description=("maps - MaRDI Packaging System : "
-                 "Provides a unified interface for packaging and deploying software environments."
-                 ),
-)
-parser.add_argument('--version', action='version', version=VERSION)
-parser.add_argument('-c', '--commit', dest='COMMIT', nargs=2, metavar=("TREE", "BRANCH"),
-                    default=False, help="Commit TREE to BRANCH in REPO")
-parser.add_argument('-i', '--initialize', dest='DIR',
-                    help="initialize DIR with a good base tree")
-parser.add_argument('-d', '--deploy', dest='DEPLOY', action='store',
-                    default=False, help="deploy mode, for installing environments")
-parser.add_argument('-p', '--package', dest='PACKAGE', action='store_true',
-                    default=False, help="package mode for defining and publishing new environments")
-parser.add_argument('--repo', dest='REPO',
-                    help="Repository to use")
-parser.add_argument('-r', '--run', dest='RUN', action='store',
-                    default=False, help="Repository to use")
-parser.add_argument('-s', '--sandbox', dest='LOCATION',
-                    help="Start a sandbox at LOCATION")
-parser.add_argument('-v', '--verbose', dest='VERBOSE', action='store_true',
-                    help="enable verbose output")
-args = parser.parse_args()
+def addCLI():
+    """Function adds a CLI to the package."""
+    parser = argparse.ArgumentParser(
+        prog='maps',
+        description=("maps - MaRDI Packaging System : "
+                     "Provides a unified interface for packaging "
+                     "and deploying software environments."
+                     ),
+    )
+    parser.add_argument('--version', action='version', version=VERSION)
+    parser.add_argument('-c', '--commit', dest='COMMIT', nargs=2, metavar=("TREE", "BRANCH"),
+                        default=False, help="Commit TREE to BRANCH in REPO")
+    parser.add_argument('-i', '--initialize', dest='DIR',
+                        help="initialize DIR with a good base tree")
+    parser.add_argument('-d', '--deploy', dest='DEPLOY', action='store',
+                        default=False, help="deploy mode, for installing environments")
+    parser.add_argument('-p', '--package', dest='PACKAGE', action='store_true',
+                        default=False, help="package mode for defining "
+                        "and publishing new environments")
+    parser.add_argument('--repo', dest='REPO',
+                        help="Repository to use")
+    parser.add_argument('-r', '--run', dest='RUN', action='store',
+                        default=False, help="Repository to use")
+    parser.add_argument('-s', '--sandbox', dest='LOCATION',
+                        help="Start a sandbox at LOCATION")
+    parser.add_argument('-v', '--verbose', dest='VERBOSE', action='store_true',
+                        help="enable verbose output")
+    return parser
 
-# Some sanity checks
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
-if not args.PACKAGE and args.RUN is False:
-    if args.DEPLOY is False:
-        raise AssertionError("This is an impossible case, I think?")
-    if args.DIR is not None:
-        raise AssertionError("Can only initialize a dir in package mode!")
-    if args.LOCATION is not None:
-        raise AssertionError("As of now, sandboxing is only supported as a part of packaging.")
-    if args.COMMIT is not False:
-        raise AssertionError("Can only commit to repo in package mode!")
-else:
-    if args.DEPLOY is not False:
-        raise AssertionError("Cannot open in package mode and deploy mode simultaneously! "
-                             "Please check arguments!")
-
-# Globals
-if os.getenv('XDG_DATA_HOME') is not None:
-    data = os.getenv('XDG_DATA_HOME')
-else:
-    # this will crash if HOME is not set. How likely?
-    data = f"{os.getenv('HOME')}/.local/share"
-data = f"{data}/org.mardi.maps"
-if args.REPO is None:
-    repopath = f"{data}/ostree/repo"
-else:
-    repopath = args.REPO
-repo = repopath.split('/')[-1]
-repopath = '/'.join(repopath.split('/')[0:-1])
 
 # if the directory does not exist, assume we're doing a first run, go through initialization
-if not os.path.isdir(repopath):
+def program_init(repopath):
+    """Function verifies requirements, and initializes the working directories"""
     # step 1 : check bwrap, and overlayfs-fuse are installed
     assert os.path.isfile(BWRAP)
     assert os.path.isfile("/usr/bin/fuse-overlayfs")
     # step 2 : create the directory, so the function is not called again
     subprocess.run(f"mkdir -pv {'/'.join(repopath.split('/'))}".split(), check=True)
 
-fd = os.open(repopath, os.O_RDONLY)
-repo = OSTree.Repo.create_at(fd, repo,
-                             OSTree.RepoMode(OSTREE_REPO_MODE_BARE_USER), None, None)
-repo.open(None)
-# Configure a good known remote, if not already present
-if (not repo.remote_list()) or "NameOfRemote" not in repo.remote_list():
-    repo.remote_add("NameOfRemote", "http://maunzerle:81",
-                    GLib.Variant('a{sv}', {"gpg-verify": GLib.Variant('b', False)}), None)
 
-# Run mode
-if args.RUN is not False:
+def mode_run(args):
+    """Function to execute a published environment"""
     # check if the path exists
     DATADIR = f"{os.getenv('HOME')}/.var/org.mardi.maps/{args.RUN}"
-    PDATADIR = '/'.join(DATADIR.split('/')[0:-1])
     if not os.path.isdir(DATADIR):
         raise AssertionError(f"Data directory does not exist. Is {args.RUN} installed ?")
     # setup live directory
@@ -105,13 +71,16 @@ if args.RUN is not False:
     rstatus = subprocess.run([BWRAP, "--no-int-term", "--unshare-user", "--unshare-pid",
                               "--bind", f"{DATADIR}/live", "/", "--proc", "/proc", "--dev", "/dev",
                               "--uid", "0", "--gid", "0", "bash"], check=False)
-
+    if rstatus.returncode != 0:
+        print(f"Sandbox exited with return code {rstatus.returncode}")
     # when the sandbox exits, cleanup
     # can this fail? how do we handle that scenario?
     subprocess.run(["fusermount", "-u", f"{DATADIR}/live"], check=False)
 
+
 # Deploy Mode
-elif args.DEPLOY is not False:
+def mode_deploy(repo, args):
+    """Function to deploy from repo to local disk"""
     # Right now, we only checkout things from the local ostree repo
     # Later, we also need to check from a trusted remote, if not found in the local repo
     DATADIR = f"{os.getenv('HOME')}/.var/org.mardi.maps/{args.DEPLOY}"
@@ -129,8 +98,10 @@ elif args.DEPLOY is not False:
     repo.checkout_at(None, tfd, "rofs", refhash, None)
     print(f"Success... {args.DEPLOY} is now ready to use!")
 
+
 # Package Mode
-else:
+def mode_package(repo, args):
+    """Function for package mode. Not intended to be used by "end users" """
     if args.DIR is not None:
         refhash = ''
         if 'base/x86_64/debian' not in list(repo.list_refs()[1].keys()):
@@ -167,5 +138,75 @@ else:
         print(mcommit[1])
         repo.transaction_set_ref(None, args.COMMIT[1], mcommit[1])
         repo.commit_transaction(None)
-        status, refs = repo.list_refs()
+        _, refs = repo.list_refs()
         print(list(refs.keys()))
+
+
+def sanity_checks(parser, args):
+    """Some simply sanity checks, before the program proceeds"""
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    if not args.PACKAGE and args.RUN is False:
+        if args.DEPLOY is False:
+            raise AssertionError("This is an impossible case, I think?")
+        if args.DIR is not None:
+            raise AssertionError("Can only initialize a dir in package mode!")
+        if args.LOCATION is not None:
+            raise AssertionError("As of now, sandboxing is only supported as a part of packaging.")
+        if args.COMMIT is not False:
+            raise AssertionError("Can only commit to repo in package mode!")
+    else:
+        if args.DEPLOY is not False:
+            raise AssertionError("Cannot open in package mode and deploy mode simultaneously! "
+                                 "Please check arguments!")
+
+
+# Main function
+def main():
+    """Main function"""
+    parser = addCLI()
+    args = parser.parse_args()
+
+    # Some sanity checks
+    sanity_checks(parser, args)
+
+    # Setup
+    if os.getenv('XDG_DATA_HOME') is not None:
+        data = os.getenv('XDG_DATA_HOME')
+    else:
+        # this will crash if HOME is not set. How likely?
+        data = f"{os.getenv('HOME')}/.local/share"
+    data = f"{data}/org.mardi.maps"
+    if args.REPO is None:
+        repopath = f"{data}/ostree/repo"
+    else:
+        repopath = args.REPO
+    repo = repopath.split('/')[-1]
+    repopath = '/'.join(repopath.split('/')[0:-1])
+
+    if not os.path.isdir(repopath):
+        program_init(repopath)
+
+    fd = os.open(repopath, os.O_RDONLY)
+    repo = OSTree.Repo.create_at(fd, repo,
+                                 OSTree.RepoMode(OSTREE_REPO_MODE_BARE_USER), None, None)
+    repo.open(None)
+    # Configure a good known remote, if not already present
+    if (not repo.remote_list()) or "NameOfRemote" not in repo.remote_list():
+        repo.remote_add("NameOfRemote", "http://maunzerle:81",
+                        GLib.Variant('a{sv}', {"gpg-verify": GLib.Variant('b', False)}), None)
+
+    # Run mode
+    if args.RUN is not False:
+        mode_run(args)
+    # Deploy mode
+    elif args.DEPLOY is not False:
+        mode_deploy(repo, args)
+    # Package mode
+    else:
+        mode_package(repo, args)
+
+
+if __name__ == "__main__":
+    main()
