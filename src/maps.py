14b1611d563cd36594849cf1244feb7d81ae5f61
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-09-14 14:00:14 +0200

    Bugfix: forgot to increment the counter

--- src/maps.py
+++ src/maps.py
@@ -224,6 +224,7 @@ def download(args, repo, remote, refhash, cerror=0):
             if cerror > 10:
                 print("10 consecutive network failures. Bailing!")
                 future.result()
+            cerror = cerror + 1
             print(f"Retrying... ({cerror}/10)")
             download(args, repo, remote, refhash, cerror)
         else:
