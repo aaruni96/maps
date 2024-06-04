49d72a71db1d6d80475cda0e6b8bf63f9457736d
Aaruni Kaushik <aaruni@edufor.me>
2024-06-04 17:35:11 +0200

    add lots of debug output

--- pkg/rpm/build-rpm.sh
+++ pkg/rpm/build-rpm.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
+set -e
+
+echo "Setting up rpmbuild dir"
 rpmdev-setuptree
 
+echo "cd to rpmbuild"
 cd "$HOME/rpmbuild"
+ls
 
+echo "cloning maps"
 git clone "https://github.com/aaruni96/maps"
+ls 
 
 VERSION=$(cat maps/Readme.md | grep -i 'version' | head -n 1 | sed 's/^.*version-//' | sed 's/-.*//').0.1
 
+echo "VERSION is $VERSION"
+
 mv maps maps-$VERSION
 
-cd "$HOME/rpmbuild/maps-$VERSION"
+#cd "$HOME/rpmbuild/maps-$VERSION"
 
 # figure out changelogs later
 #git log --format="* %ad %an - %s%b%n" $(git rev-list --tags --max-count=1)..HEAD >> pkg/rpm/maps.spec
 
-cd "$HOME/rpmbuild"
+#cd "$HOME/rpmbuild"
 
 tar -czf maps-$VERSION.tar.gz maps-$VERSION
 
 mv maps-$VERSION.tar.gz SOURCES
 
-cp maps-$VERSION/pkg/rpm/maps.spec SPECS/maps.spec
+echo "trying to move SPECfile to right place"
+echo "step1 : list specfile"
+ls "maps-$VERSION"
+ls "maps-$VERSION/pkg"
+ls "maps-$VERSION/pkg/rpm"
+cp -v maps-$VERSION/pkg/rpm/maps.spec SPECS/maps.spec
 
 rpmbuild -bb ~/rpmbuild/SPECS/maps.spec
