225b2f445c0bcea2287026d34e5fb6999b69baa7
Aaruni Kaushik <aaruni@edufor.me>
2024-06-17 15:11:33 +0200

    cp needs -r for directories

--- pkg/rpm/build-rpm.sh
+++ pkg/rpm/build-rpm.sh
@@ -11,7 +11,7 @@ echo "cd to rpmbuild"
 cd "$HOME/rpmbuild"
 ls
 
-cp -v $MAPSDIR ./
+cp -rv $MAPSDIR ./
 ls 
 
 VERSION=$(cat maps/Readme.md | grep -i 'version' | head -n 1 | sed 's/^.*version-//' | sed 's/-.*//').0.1
