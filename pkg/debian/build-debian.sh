de0c61adcaf93d47c8a529513eca74c40ee418ed
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2024-06-20 15:30:05 +0200

    Update debian building
    [skip ci]

--- pkg/debian/build-debian.sh
+++ pkg/debian/build-debian.sh
@@ -36,62 +36,20 @@ cd "maps_${VERSION}" && mkdir -pv "debian/source"
 
 echo "3.0 (quilt)" > "debian/source/format"
 
-echo "maps (0.1-2) UNRELEASED; urgency=medium
-
-  * Update to v0.1
-
-  * Initial release.
-
- -- Aaruni Kaushik  <akaushik@mathematik.uni-kl.de>  $(date +'%a, %d %b %Y %H:%M:%S %z')" > "debian/changelog"
+cp -v pkg/debian/changelog debian/changelog
 
 # add control
 
-echo 'Source: maps
-Maintainer: Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
-Section: misc
-Priority: optional
-Standards-Version: 4.6.0.1
-Build-Depends: debhelper-compat (= 13)
-
-Package: maps
-Architecture: amd64
-Depends: ${shlibs:Depends}, ${misc:Depends}, libcairo2-dev, git, gcc (>= 11), python3, python3-dev, libgirepository1.0-dev, libostree-dev, fuse-overlayfs, python3-venv, libcap-dev, autoconf, python3-gi, python3-tomli
-Description: Mathematical Packaging System
- MaPS helps mathematicians create and publish software runtimes, 
- as well as deploy and run software inside published runtimes.' > "debian/control"
+cp -v pkg/debian/control debian/control
 
 # add copyright
 
-echo 'Format: http://dep.debian.net/deps/dep5
-Upstream-Name: MaPS
-Source: https://github.com/aaruni96/maps
-
-Files: *
-Copyright: 2024 Aaruni Kaushi
-License: GPL-3.0
-
-License: GPL-3.0
- This package is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- .
- This package is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- .
- You should have received a copy of the GNU General Public License
- along with this package; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- .
- On Debian systems, the complete text of the GNU General
- Public License can be found in `/usr/share/common-licenses/GPL-3.`' > "debian/copyright"
+cp -v pkg/debian/copyright debian/copyright
 
 # debian.dirs
 
-echo "usr/bin
-usr/share/bash-completion/completions" > "debian/maps.dirs"
+echo "usr/bin" > "debian/maps.dirs"
+echo "usr/share/bash-completion/completions" >> "debian/maps.dirs"
 
 # debian rules
 
