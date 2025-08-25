30707c3fd7b667b08f6695a5278430c5fda0914c
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2025-08-25 15:24:14 +0200

    Test running, and public passthroughj

--- src/test_maps.py
+++ src/test_maps.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+import pathlib
 import subprocess
 
 def test_list():
@@ -12,6 +14,21 @@ def test_ambiguous_deploy():
 def test_ambiguous_update():
     a = subprocess.run("python src/maps --update base/x86_64/debian", shell=True, capture_output=True)
     assert a.returncode == 0
+
+def test_ambigious_run():
+    a = subprocess.run(["python", "src/maps", "--run", "base/x86_64/debian", "--command",
+                        "bash -c exit"], shell=False, capture_output=True)
+    assert a.returncode == 0
+
+def test_public_passthrough():
+    pubdirpath = f"{os.getenv('HOME')}/Public"
+    testfilename = 'maps-test-file.txt'
+    pathlib.Path(pubdirpath).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(f"{pubdirpath}/{testfilename}").touch()
+    a = subprocess.run(["python", "src/maps", "--run", "base/x86_64/debian", "--command",
+                        f"ls Public/{testfilename}"], shell=False, capture_output=True)
+    assert a.returncode == 0
+    pathlib.Path(f"{pubdirpath}/{testfilename}").unlink()
 
 def test_ambiguous_reset():
     a = subprocess.run("python src/maps --reset base/x86_64/debian", shell=True, capture_output=True)
@@ -27,6 +44,11 @@ def test_exact_deploy():
 
 def test_exact_update():
     a = subprocess.run("python src/maps --update Official:base/x86_64/debian", shell=True, capture_output=True)
+    assert a.returncode == 0
+
+def test_exact_run():
+    a = subprocess.run(["python", "src/maps", "--run", "Official:base/x86_64/debian", "--command",
+                        "bash -c exit"], shell=False, capture_output=True)
     assert a.returncode == 0
 
 def test_exact_reset():
